db9ccd708a58fb22d2596d718575ea1a069d1f10
“margaritisarg” <margaritis1argyrou@gmail.com>
2023-03-27 16:42:21 +0100

    testing merge on ticket4

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -1,5 +1,6 @@
 const { fetchReviewByID } = require("../models/reviews.model.js");
 
+//test
 exports.getReviewsByID = (req, res, next) => {
     fetchReviewByID(req.params.review_id)
         .then(data => {
