eff16f5b2c4b4bc4938adb0d1b7c045314d3dac0
“margaritisarg” <margaritis1argyrou@gmail.com>
2023-03-30 10:22:27 +0100

    Corrections for Ticket7 and hosting stuffSection6

--- controllers/errorHandling.controller.js
+++ controllers/errorHandling.controller.js
@@ -1,18 +1,17 @@
 
 exports.errorHandle = (err, req, res, next) => {   
-    if(err.status && err.msg){
+    if(err.code === "22P02"){
+        res.status(400).send({errorCode: err.code, msg: "invalid syntax type"})
+    }
+    else if(err.status && err.msg){
         res.status(err.status).send({msg: err.msg});
     }
     else if(err.code === '23503'){
-        let columnFK = (/[(]\w+[)]=/.exec(err.detail))[0]
-        columnFK = columnFK.substring(1, columnFK.length-2)
-        if(columnFK.includes("_")) columnFK = (columnFK.split("_"))[0];
-
         let input = (/[(]\w+[)] /.exec(err.detail))[0]
         input = input.substring(1, input.length-2)
 
-        const errorMsg = `${columnFK} does not have a record of: '${input}' in its corresponding table`;
-        res.status(400).send({post_error: errorMsg})
+        const errorMsg = `${input} not found`;
+        res.status(404).send({status: 404, msg: errorMsg})
     }
     else{
         next(err)
